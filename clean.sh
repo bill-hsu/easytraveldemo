116ce23a083d8e3b42cd1f96b131bd7ad749cfe8
Martin Etmajer <m.etmajer@gmail.com>
2016-07-26 14:27:31 +0200

    Initial commit.

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for name in 'easytravel-mongodb' 'easytravel-backend' 'easytravel-frontend' 'easytravel-www'; do
-  oc delete dc ${name}
-  oc delete is ${name}
-  oc delete svc ${name}
-done
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-. ./config/os-settings.sh
-
-oc login https://${OS_MASTER_IP}:8443 --username=admin --password=admin
-oc create -f templates/dynatrace-easytravel.yml
-oc new-app dynatrace-easytravel
