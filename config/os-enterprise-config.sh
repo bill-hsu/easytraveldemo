f58cdcfb35c9d181df8927eeed6cd8ea2ad87800
Martin Etmajer <m.etmajer@gmail.com>
2016-08-17 21:02:50 +0200

    Set instance type to m3.medium.

--- config/os-enterprise-config.sh
+++ config/os-enterprise-config.sh
@@ -2,6 +2,7 @@
 export ec2_image=ami-a8d369c0
 export ec2_keypair=openshift-origin-dev
 export ec2_security_groups=openshift-dev
+export ec2_instance_type=m3.medium
 export ec2_vpc_subnet=
 export rhel_subscription_user=
 export rhel_subscription_pass=
--- config/os-origin-config.sh
+++ config/os-origin-config.sh
@@ -2,4 +2,5 @@
 export ec2_image=ami-6d1c2007
 export ec2_keypair=openshift-origin-dev
 export ec2_security_groups=openshift-dev
+export ec2_instance_type=m3.medium
 export ec2_vpc_subnet=
